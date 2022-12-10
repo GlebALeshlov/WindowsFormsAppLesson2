2c0412bbf5682376518f98ec128c9209a91f8c98
GlebALeshlov <dimadima0805198585@gmail.com>
2022-12-10 11:34:37 +0300

    Initial commit

---
+++ WindowsFormsAppLesson2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppLesson2
+{
+    //Это метод инициализации
+    public partial class Form1 : Form
+    {
+        //Это метод загрузки формы
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       //Это
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "=^_^=";
+            button1.BackColor= Color.Red;
+            button1.Enabled= false;
+            label1.Text = textBox1.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "НЕ НАЖИМАЙ!!!";
+            label1.Text = "Made by Кто-то";
+            textBox1.Text = "Сотри тут";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button1.BackColor= Color.Orange;
+            button1.Text = "НЕ НАЖИМАЙ!!!";
+        }
+    }
+}
